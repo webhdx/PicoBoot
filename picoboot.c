2ccf2e018b04009d72a2bbff9d5ec36ffda100d4
Maciej <webhdx@gmail.com>
2022-06-20 20:48:05 +0200

    Initial version of PicoBoot

---
+++ picoboot.c
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2022 Maciej Kobus
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/dma.h"
+#include "hardware/structs/bus_ctrl.h"
+#include "gcipl.pio.h"
+#include "ipl.h"
+
+const uint PIN_LED = 25;                // Status LED
+const uint PIN_DATA_BASE = 6;           // Base pin used for output, 4 consecutive pins are used 
+const uint PIN_CS = 19;                 // U10 chip select
+const uint PIN_CLK = 20;                // EXI bus clock line
+
+void main()
+{
+    // Initialize and light up builtin LED, it will basically
+    // act as a power LED.
+    // TODO: Use the LED to signalize system faults?
+    gpio_init(PIN_LED);
+    gpio_set_dir(PIN_LED, GPIO_OUT);
+    gpio_put(PIN_LED, true);
+
+    // Set 250MHz clock to get more cycles in between CLK pulses.
+    // This is the lowest value I was able to make the code work.
+    // Should be still considered safe for most Pico boards.
+    set_sys_clock_khz(250000, true);
+
+    // Prioritize DMA engine as it does the most work
+    bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
+
+    gpio_set_slew_rate(PIN_DATA_BASE, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(PIN_DATA_BASE + 1, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(PIN_DATA_BASE + 2, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(PIN_DATA_BASE + 3, GPIO_SLEW_RATE_FAST);
+
+    gpio_set_drive_strength(PIN_DATA_BASE, GPIO_DRIVE_STRENGTH_4MA);
+    gpio_set_drive_strength(PIN_DATA_BASE + 1, GPIO_DRIVE_STRENGTH_4MA);
+    gpio_set_drive_strength(PIN_DATA_BASE + 2, GPIO_DRIVE_STRENGTH_4MA);
+    gpio_set_drive_strength(PIN_DATA_BASE + 3, GPIO_DRIVE_STRENGTH_4MA);
+
+    PIO pio = pio0;
+
+    //
+    // State Machine: Transfer Start
+    //
+    // Counts all consecutive transfers and sets IRQ 
+    // when first 1 kilobyte transfer starts.
+    //
+
+    uint transfer_start_sm = pio_claim_unused_sm(pio, true);
+    uint transfer_start_offset = pio_add_program(pio, &on_transfer_program);
+
+    on_transfer_program_init(pio, transfer_start_sm, transfer_start_offset, PIN_CLK, PIN_CS, PIN_DATA_BASE);
+
+    pio_sm_put(pio, transfer_start_sm, (uint32_t) 224); // CS pulses
+    pio_sm_exec(pio, transfer_start_sm, pio_encode_pull(true, true));
+    pio_sm_exec(pio, transfer_start_sm, pio_encode_mov(pio_x, pio_osr));
+    pio_sm_exec(pio, transfer_start_sm, pio_encode_out(pio_null, 32));
+
+    //
+    // State Machine: Clocked Output
+    // 
+    // It waits for IRQ signal from first SM and samples clock signal
+    // to output IPL data bits.
+    //
+
+    uint clocked_output_sm = pio_claim_unused_sm(pio, true);
+    uint clocked_output_offset = pio_add_program(pio, &clocked_output_program);
+
+    clocked_output_program_init(pio, clocked_output_sm, clocked_output_offset, PIN_DATA_BASE, PIN_CLK, PIN_CS);
+
+    pio_sm_put(pio, clocked_output_sm, 8191); // 8192 bits, 1024 bytes, minus 1 because counting starts from 0 
+    pio_sm_exec(pio, clocked_output_sm, pio_encode_pull(true, true));
+    pio_sm_exec(pio, clocked_output_sm, pio_encode_mov(pio_y, pio_osr));
+    pio_sm_exec(pio, clocked_output_sm, pio_encode_out(pio_null, 32));
+
+    // Set up DMA for reading IPL to PIO FIFO
+    int chan = dma_claim_unused_channel(true);
+
+    dma_channel_config c = dma_channel_get_default_config(chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
+    channel_config_set_read_increment(&c, true);
+    channel_config_set_write_increment(&c, false);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, clocked_output_sm, true));
+
+    dma_channel_configure(
+        chan,
+        &c,
+        &pio->txf[clocked_output_sm],
+        ipl,
+        count_of(ipl),
+        true // start immediately
+    );
+
+    // Start PIO state machines
+    pio_sm_set_enabled(pio, transfer_start_sm, true);
+    pio_sm_set_enabled(pio, clocked_output_sm, true);
+
+    while (true) {
+        tight_loop_contents();
+    }
+}
---
+++ processipl.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+#
+# Based on dol2ipl.py from https://github.com/redolution/iplboot
+#
+# Original source by 9ary, bootrom descrambler reversed by segher
+#
+
+from datetime import datetime
+import math
+import struct
+import sys
+
+payload_padding = [
+    0x81, 0x4a, 0xe6, 0xc8, 0x00, 0x04, 0xc5, 0x77, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+]
+
+def scramble(data):
+    acc = 0
+    nacc = 0
+
+    t = 0x2953
+    u = 0xD9C2
+    v = 0x3FF1
+
+    x = 1
+
+    it = 0
+    while it < len(data):
+        t0 = t & 1
+        t1 = (t >> 1) & 1
+        u0 = u & 1
+        u1 = (u >> 1) & 1
+        v0 = v & 1
+
+        x ^= t1 ^ v0
+        x ^= u0 | u1
+        x ^= (t0 ^ u1 ^ v0) & (t0 ^ u0)
+
+        if t0 == u0:
+            v >>= 1
+            if v0:
+                v ^= 0xB3D0
+
+        if t0 == 0:
+            u >>= 1
+            if u0:
+                u ^= 0xFB10
+
+        t >>= 1
+        if t0:
+            t ^= 0xA740
+
+        nacc = (nacc + 1) % 256
+        acc = (acc * 2 + x) % 256
+        if nacc == 8:
+            data[it] ^= acc
+            nacc = 0
+            it += 1
+
+    return data
+
+def flatten_dol(data):
+    header = struct.unpack(">64I", data[:256])
+
+    dol_min = min(a for a in header[18:36] if a)
+    dol_max = max(a + s for a, s in zip(header[18:36], header[36:54]))
+
+    img = bytearray(dol_max - dol_min)
+
+    for offset, address, length in zip(header[:18], header[18:36], header[36:54]):
+        img[address - dol_min:address + length - dol_min] = data[offset:offset + length]
+
+    # Entry point, load address, memory image
+    return header[56], dol_min, img
+
+def bytes_to_c_array(data):
+    p_list = [data[i:i + 4] for i in range(0, len(data), 4)]
+    return ["0x%08x" % int.from_bytes(b, byteorder='big', signed=False) for b in p_list]
+
+def generate_header_file(elements, executable, input_file, output_file, size):
+    output = '#include <stdio.h>\n\n'
+    output += '//\n'
+    output += '// Command: {0} {1} {2}\n'.format(executable, input_file, output_file)
+    output += '//\n'
+    output += '// File: {0}, size: {1}K\n'.format(input_file, size)
+    output += '//\n'
+    output += '// File generated on {0}\n'.format(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+    output += '//\n\n'
+    output += 'uint32_t __in_flash("ipl_data") ipl[]  = {\n\t'
+
+    for num in range(len(elements)):
+        if num > 0 and num % 4 == 0:
+            output += '\n\t'
+        
+        output += elements[num]
+
+        if num != len(elements):
+            output += ', '
+
+    output += '\n};\n'
+
+    return output
+
+def process_scrambled_ipl(ipl, size):
+    """Does additional processing to scrambled IPL payload.
+
+    Payload used by PicoBoot has to be preprocessed. 
+    Whole payload has to be aligned to 1K blocks then every bit needs to be duplicated 4 times.
+    """
+    
+    out2 = int.from_bytes(ipl, byteorder='big', signed=False)
+    out2 = out2 << 1
+
+    binary = ''.join([char*4 for char in format(out2, 'b')])
+    binary = int(binary, 2)
+
+    payload = binary.to_bytes(size * 4, 'big')
+    
+    return payload
+    
+def main():
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <executable> <output>")
+
+    with open(sys.argv[1], "rb") as f:
+        exe = bytearray(f.read())
+
+    if sys.argv[1].endswith(".dol"):
+        entry, load, img = flatten_dol(exe)
+        entry &= 0x017FFFFF
+        entry |= 0x80000000
+        load &= 0x017FFFFF
+        size = len(img)
+
+        print(f"Entry point:   0x{entry:0{8}X}")
+        print(f"Load address:  0x{load:0{8}X}")
+        print(f"Image size:    {size} bytes ({size // 1024}K)")
+    else:
+        print("Unknown input format")
+        return -1
+
+    if entry != 0x81300000 or load != 0x01300000:
+        print("Invalid entry point and base address (must be 0x81300000)")
+        return -1
+
+    ipl = bytearray(0x700)
+    ipl += bytearray(payload_padding)
+    ipl += img
+
+    ipl_size = len(ipl)
+
+    print(f"Output binary size: {ipl_size} bytes ({ipl_size / 1024}K)")
+
+    if ipl_size % 1024 != 0:
+        new_size = math.ceil(ipl_size / 1024)
+        print(f"Payload needs to be aligned to {new_size}K")
+        ipl += bytearray(1024 - (ipl_size % 1024))
+        ipl_size = new_size
+
+    payload = scramble(ipl)[0x700:]
+    payload_size = len(payload);
+
+    elements = bytes_to_c_array(process_scrambled_ipl(payload, payload_size))
+
+    output = generate_header_file(elements, sys.argv[0], sys.argv[1], sys.argv[2], ipl_size)
+
+    with open(sys.argv[2], "w") as f:
+        f.write(output)
+
+if __name__ == "__main__":
+    sys.exit(main())
