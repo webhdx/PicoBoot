1c8a7b796831bf9df8743fe7f3d446ae746daf7a
Maciej Kobus <webhdx@gmail.com>
2024-08-07 12:55:17 +0200

    Fill empty uf2 blocks to align with 4kB erase sector

--- merge_uf2.py
+++ merge_uf2.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 import itertools
+import math
 import struct
 import sys
 
@@ -58,14 +59,47 @@ def write_uf2(blocks, name):
             )
             f.write(data)
 
+def padding_block(flags, address, seq, total_blocks, family_id):
+    return Uf2Block(
+        flags,
+        address,
+        256,
+        seq,
+        total_blocks,
+        family_id,
+        bytes(476)
+    )
+
 def main():
     output = sys.argv[1]
     inputs = sys.argv[2:]
 
     inputs = (read_file(f) for f in inputs)
-    inputs = (decode_uf2(f) for f in inputs)
+    blocks = []
+    for f in inputs:
+        file_blocks = list(decode_uf2(f))
+        first_block = file_blocks[0]
+        last_block = file_blocks[-1]
+        blocks += file_blocks
+        total_size = (last_block.address + last_block.size) - first_block.address
 
-    blocks = [b for b in itertools.chain.from_iterable(inputs)]
+        if total_size % 4096 != 0:
+            new_size = (math.ceil(total_size / 4096) + 1) * 4096
+            padding_size = new_size - total_size
+            padding_blocks = int(padding_size / 256)
+            total_blocks = last_block.total_blocks + padding_blocks
+            
+            for index in range(padding_blocks):
+                seq = index + 1
+                blocks.append(
+                    padding_block(
+                        first_block.flags,
+                        last_block.address + (seq * 256),
+                        last_block.seq + seq,
+                        total_blocks,
+                        last_block.family_id
+                    )
+                )
 
     ref = blocks[0]
     for b in blocks:
